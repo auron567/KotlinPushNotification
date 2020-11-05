98af53e29630b46340f9868a2234364681795966
auron567 <xauron567x@gmail.com>
2020-11-05 18:10:51 +0100

    Check for Google Play Services

--- app/src/main/java/com/example/kotlinpushnotification/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpushnotification/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlinpushnotification.databinding.ActivityMainBinding
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.firebase.installations.FirebaseInstallations
 import timber.log.Timber
 
@@ -16,17 +18,34 @@ class MainActivity : AppCompatActivity() {
 
         binding.retrieveTokenButton.setOnClickListener {
             // Get device token
-            FirebaseInstallations.getInstance().getToken(false)
-                .addOnCompleteListener { task ->
-                    if (!task.isSuccessful) {
-                        Timber.w(task.exception)
-                        return@addOnCompleteListener
-                    }
+            if (checkGooglePlayServices()) {
+                FirebaseInstallations.getInstance().getToken(false)
+                    .addOnCompleteListener { task ->
+                        if (!task.isSuccessful) {
+                            Timber.w(task.exception)
+                            return@addOnCompleteListener
+                        }
 
-                    val token = task.result?.token
-                    Timber.d("Token: $token")
-                    Toast.makeText(this, "Token: $token", Toast.LENGTH_SHORT).show()
-                }
+                        val token = task.result?.token
+                        Timber.d("Token: $token")
+                        Toast.makeText(this, "Token: $token", Toast.LENGTH_SHORT).show()
+                    }
+            } else {
+                // Unable to send notifications to this device
+                Timber.w("Device doesn't have google play services")
+            }
+        }
+    }
+
+    private fun checkGooglePlayServices(): Boolean {
+        val status = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
+        return if (status != ConnectionResult.SUCCESS) {
+            Timber.w("Google Play services is unavailable")
+            // Ask user to update google play services
+            false
+        } else {
+            Timber.d("Google Play services is available")
+            true
         }
     }
 }
