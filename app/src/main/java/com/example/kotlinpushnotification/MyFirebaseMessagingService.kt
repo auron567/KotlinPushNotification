235c28b22373de9cd3e4a7285bafce74406d56c0
auron567 <xauron567x@gmail.com>
2020-11-17 09:12:42 +0100

    Handle notifications in the foreground

--- app/src/main/java/com/example/kotlinpushnotification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/kotlinpushnotification/MyFirebaseMessagingService.kt
@@ -1,5 +1,29 @@
 package com.example.kotlinpushnotification
 
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import timber.log.Timber
 
-class MyFirebaseMessagingService : FirebaseMessagingService()
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        Timber.d("Refreshed token: $token")
+        getSharedPreferences("_", MODE_PRIVATE).edit().putString("fcm_token", token).apply()
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Timber.d("From: ${remoteMessage.from}")
+        handleMessage()
+    }
+
+    private fun handleMessage() {
+        val handler = Handler(Looper.getMainLooper())
+        handler.post {
+            Toast.makeText(baseContext, getString(R.string.do_something), Toast.LENGTH_LONG).show()
+        }
+    }
+}
